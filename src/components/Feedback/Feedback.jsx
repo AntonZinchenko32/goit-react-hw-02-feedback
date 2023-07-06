8a12c0d07bd6418fbeca4f97fdd38531d5c53d27
AntonZinchenko32 <zinchenko309@gmail.com>
2023-07-06 23:28:03 +0300

    Разметка
    Создаёт элементы разметки

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -14,9 +14,31 @@ class Feedback extends Component {
     const { children } = this.props;
 
     return (
-      <div>
-        <button className={css.myButton} onClick={this.toggle}>{isOpen ? "Hide" : "Show"}</button>
-        {isOpen && children}
+        <div>
+            <h2>Please leave feedback</h2>
+
+        <button className={css.myButton} onClick={this.toggle}>Good</button>
+        <button className={css.myButton} onClick={this.toggle}>Neutral</button>
+        <button className={css.myButton} onClick={this.toggle}>Bad</button>
+           
+        <h2>Statistics</h2>
+
+        <ul className={css.statList}>
+            <li className={css.statListItem}>
+               <span className={css.label}>Bad:</span>
+               <span className={css.percentage}>0</span>     
+                </li>
+                
+            <li className={css.statListItem}>
+               <span className={css.label}>Neutral:</span>
+               <span className={css.percentage}>0</span>     
+                </li>
+                
+            <li className={css.statListItem}>
+               <span className={css.label}>Good:</span>
+               <span className={css.percentage}>0</span>     
+            </li>    
+        </ul>    
       </div>
     );
   }
