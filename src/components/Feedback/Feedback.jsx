fd3c6dd027e7eb96934331686969fe2a83150d47
AntonZinchenko32 <zinchenko309@gmail.com>
2023-07-07 00:20:28 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,42 +3,47 @@ import css from './Feedback.module.css';
 
 
 class Feedback extends Component {
-  state = { isOpen: false };
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
 
-  toggle = () => {
-      this.setState(state => ({ isOpen: !state.isOpen }));
+  addFeedback = () => {
+      this.setState(state => ({
+          
+      }));
   };
 
   render() {
-    const { isOpen } = this.state;
-    const { children } = this.props;
+    
 
     return (
         <div>
             <h2>Please leave feedback</h2>
 
-        <button className={css.myButton} onClick={this.toggle}>Good</button>
-        <button className={css.myButton} onClick={this.toggle}>Neutral</button>
-        <button className={css.myButton} onClick={this.toggle}>Bad</button>
-           
-        <h2>Statistics</h2>
+            <button className={css.myButton} onClick={this.addFeedback}>Good</button>
+            <button className={css.myButton} onClick={this.addFeedback}>Neutral</button>
+            <button className={css.myButton} onClick={this.addFeedback}>Bad</button>
+        
+            <h2>Statistics</h2>
 
-        <ul className={css.statList}>
-            <li className={css.statListItem}>
-               <span className={css.label}>Bad:</span>
-               <span className={css.percentage}>0</span>     
+            <ul className={css.statList}>
+                <li className={css.statListItem}>
+                    <span className={css.label}>Bad:</span>
+                    <span className={css.percentage}>{this.state.bad}</span>     
                 </li>
                 
-            <li className={css.statListItem}>
-               <span className={css.label}>Neutral:</span>
-               <span className={css.percentage}>0</span>     
+                <li className={css.statListItem}>
+                    <span className={css.label}>Neutral:</span>
+                    <span className={css.percentage}>{this.state.neutral}</span>     
                 </li>
                 
-            <li className={css.statListItem}>
-               <span className={css.label}>Good:</span>
-               <span className={css.percentage}>0</span>     
-            </li>    
-        </ul>    
+                <li className={css.statListItem}>
+                    <span className={css.label}>Good:</span>
+                    <span className={css.percentage}>{this.state.good}</span>     
+                </li>    
+            </ul>    
       </div>
     );
   }
