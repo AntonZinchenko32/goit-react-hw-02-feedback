8ad8ac2b1624fd452641e45579d5181c1be0b460
AntonZinchenko32 <zinchenko309@gmail.com>
2023-08-05 02:04:42 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
+
 import css from './FeedbackOptions.module.css';
 import PropTypes from "prop-types"
-import { nanoid } from "nanoid";
 
-export default class FeedbackOptions extends Component {
-    render() {
 
-        const {
+export default function FeedbackOptions ({onLeaveFeedback, options}) {
+    
+    const {
             myButtons,
             myButton
         } = css;
 
-        const { onLeaveFeedback, options } = this.props;
         
-        // Пишемо функцію, що збільшує першу літеру у рядку, для назв кнопок
-        function capitalizeFirstLetter(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-        
-        return (
-            <div className={myButtons}>
-                {options.map(option => (
-                    <button key={nanoid()} className={myButton} onClick={onLeaveFeedback}>{capitalizeFirstLetter(option)}</button>
-                ))}
-            </div>
-        )
+    // Пишемо функцію, що збільшує першу літеру у рядку, для назв кнопок
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
+
+    return (
+        <div className={myButtons}>
+            {options.map(option => (
+                <button key={option} className={myButton} onClick={onLeaveFeedback}>{capitalizeFirstLetter(option)}</button>
+            ))}
+        </div>
+    )       
 }
 
+
+
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string)
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired
 }
